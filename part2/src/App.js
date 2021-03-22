8967646a59951e86667089556f4a503244e303e8
royderks <10717410+royderks@users.noreply.github.com>
2021-03-22 18:20:00 +0100

    Ex-7 solution

--- part2/src/App.js
+++ part2/src/App.js
@@ -9,33 +9,34 @@ import Characters from './components/Characters/Characters';
 
 function App() {
   return (
-    <div className='main'>
-      <Header />
-      <div className='intro'>
-        <p>
-          The code for this workshop uses the{' '}
-          <a
-            href='https://rickandmortyapi.com/documentation'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            The Rick and Morty API
-          </a>
-          , which is a RESTful and GraphQL API based on the television show Rick
-          and Morty. You will have access to about hundreds of characters,
-          images, locations and episodes. The Rick and Morty API is filled with
-          canonical information as seen on the TV show.
-        </p>
-      </div>
-      <Router>
-        <Switch>
-        <Route path="/characters" component={Characters} />
-        <Route path="/" component={Characters} />
-        </Switch>
-      </Router>
+    <Router> {/* IMPORTANT ROUTER MUST WRAP ENTIRE COMPONENT */}
+      <div className='main'>
+        <Header />
+        <div className='intro'>
+          <p>
+            The code for this workshop uses the{' '}
+            <a
+              href='https://rickandmortyapi.com/documentation'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              The Rick and Morty API
+            </a>
+            , which is a RESTful and GraphQL API based on the television show
+            Rick and Morty. You will have access to about hundreds of
+            characters, images, locations and episodes. The Rick and Morty API
+            is filled with canonical information as seen on the TV show.
+          </p>
+        </div>
 
-      <Footer />
-    </div>
+        <Switch>
+          <Route path='/characters' component={Characters} />
+          <Route path='/' component={Characters} />
+        </Switch>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
