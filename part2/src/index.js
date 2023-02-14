16a86651665aea87bbf556cce455d5c7b75532c7
royderks <10717410+royderks@users.noreply.github.com>
2023-02-14 11:51:53 +0100

    Add ex5 solution

--- part2/src/index.js
+++ part2/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
   document.getElementById('root'),
 );
