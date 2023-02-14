5c5d15ab3d0f0fb8e3eddfb5209ad02a7361ec02
royderks <10717410+royderks@users.noreply.github.com>
2023-02-14 12:10:55 +0100

    Add ex9 solution

--- part2/src/components/Characters/Characters.js
+++ part2/src/components/Characters/Characters.js
@@ -1,25 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import Filter from '../Filter/Filter';
 import List from '../List/List';
-import './Characters.css'
+import './Characters.css';
 
 export default function Characters() {
   const [characters, setCharacters] = useState([]);
-  const [filter, setFilter] = useState('');
+  const { pathname, search } = useLocation();
+  const navigate = useNavigate();
+
+  const filter = new URLSearchParams(search).get('species') || '';
+
+  function setFilter(filter) {
+    navigate(`${pathname}?species=${filter}`);
+  }
 
   useEffect(() => {
     function getCharacters() {
-      fetch('https://rickandmortyapi.com/api/character')
+      fetch(`https://rickandmortyapi.com/api/character?species=${filter}`)
         .then((result) => result.json())
         .then((data) => {
-        if (filter) {
-          return setCharacters(
-            data.results.filter((result) => result.species === filter),
-          );
-        }
-
-        return setCharacters(data.results);
-      });
+          return setCharacters(data.results);
+        });
     }
 
     getCharacters();
